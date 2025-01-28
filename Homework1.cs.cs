b7c484d75c6ab73a6a9abb5ff60ad86ccbfc07a9
ntala12 <nntalavera1@buffs.wtamu.edu>
2025-01-27 19:15:11 -0800

    Homework 1 code file

---
+++ Homework1.cs.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x = 2.5;
+        double y = 3.3;
+        double z = 4 * x * x + 3 * y;
+        Console.WriteLine("X = 2.5, Y = 3.3");
+        Console.Write("The value of Z is:");
+        Console.Write( 4 * x * x + 3 * y);
+    
+    
+
+    
+    }
+}
