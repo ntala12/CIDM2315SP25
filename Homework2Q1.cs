02c77ede02ddb9bff7bf0e6eeb1402bb1b0c3a09
ntala12 <nntalavera1@buffs.wtamu.edu>
2025-02-03 21:15:08 -0800

    Add files via upload

---
+++ Homework2Q1.cs
@@ -0,0 +1,36 @@
+﻿namespace Homework2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    //Question 1
+    Console.WriteLine("Please input a letter grade: ");
+       char letterGrade = 
+       Console.ReadKey().KeyChar; 
+       Console.WriteLine();
+       
+       letterGrade = char.ToUpper(letterGrade);
+       
+        switch (letterGrade)
+            { case 'A':
+                Console.WriteLine("GPA point: 4");
+            break;
+              case 'B':
+                Console.WriteLine("GPA point: 3");
+            break;
+              case 'C':
+                Console.WriteLine("GPA point: 2");
+            break;
+              case 'D':
+                Console.WriteLine("GPA point: 1");
+            break;
+              case 'F':
+                Console.WriteLine("GPA point: 0");
+            break;
+        default:
+            Console.WriteLine("Wrong Letter Grade!");
+            break;
+        }
+    }
+}
---
+++ Homework2Q2.cs
@@ -0,0 +1,33 @@
+﻿namespace Question2;
+
+class Program
+{
+    public class SmallestNumber
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Please input the first num:");
+         int num1=
+            Convert.ToInt16(Console.ReadLine());
+            
+            Console.WriteLine("Please input the second num:");
+        int num2=
+            Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Please input the third num:");
+        int num3=
+            Convert.ToInt16(Console.ReadLine());
+        
+        int smallest = num1; //Assume the first number is the smallest initially
+            if (num2 < smallest)
+            {
+                smallest = num2;
+            }
+            if (num3 < smallest)
+            {
+                smallest = num3;
+            }
+            Console.WriteLine("The smallest value is: " + smallest);
+        }
+    }
+}
