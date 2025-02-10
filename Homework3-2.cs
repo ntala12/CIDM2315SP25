bce87f9a5a2e48da882e3d1fc017a4bbc2edd6fe
ntala12 <nntalavera1@buffs.wtamu.edu>
2025-02-10 12:07:43 -0800

    Add files via upload

---
+++ Homework3-2.cs
@@ -0,0 +1,24 @@
+﻿namespace Homework3_2;
+
+class Program
+
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Assign an int value to N:");
+        int n=
+    int.Parse(Console.ReadLine());
+        //Outer loop for rows
+        for (int i = 0; i < n; i++)
+        {
+            //Inner loop for columns
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write("#");
+                //Print # for each cell
+            }
+            Console.WriteLine(); 
+            //Move to the next line after each row
+        }
+    }
+}
