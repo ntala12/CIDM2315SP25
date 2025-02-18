22f8c291e7f0714925a6e1e039abcdb86ab40733
ntala12 <nntalavera1@buffs.wtamu.edu>
2025-02-17 16:14:00 -0800

    Add files via upload

---
+++ Homework4-Q1.cs
@@ -0,0 +1,30 @@
+﻿namespace Homework4;
+
+class Program
+  {
+    static void Main(string[] args)
+    {
+        int a = 3;
+        int b = 5;
+
+        int largest =
+        LargestNumber.FindLargest(a, b); //Add LargestNumber
+
+        Console.WriteLine("a = " + a + "; b = " + b);
+        Console.WriteLine("The largest number is: " + largest);
+    }
+  }
+  public class LargestNumber
+  {
+    //Method to find the largest of two numbers
+    public static int FindLargest(int num1, int num2)
+    {
+        if (num1 > num2)
+        {
+            return num1;
+        }
+        else{
+            return num2;
+        }
+    }
+  }
