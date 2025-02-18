4a9556d38685bfb84383cb9a687228c1d665ae55
ntala12 <nntalavera1@buffs.wtamu.edu>
2025-02-17 16:44:12 -0800

    Add files via upload

---
+++ Homework4-Q2.cs
@@ -0,0 +1,26 @@
+﻿namespace Homework4_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    PrintTriangle(5, "left");
+    }
+    static void PrintTriangle(int N, string shape)
+    {
+        Console.WriteLine($"N: {N}");
+        Console.WriteLine($"Shape: {shape}");
+
+        if (shape.ToLower() == "left" && N == 5)
+        {
+            for (int i = 1; i <= N; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
