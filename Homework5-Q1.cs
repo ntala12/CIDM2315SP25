3f365d4382a5e3e3d50680a5876c6e70679d7e05
ntala12 <nntalavera1@buffs.wtamu.edu>
2025-02-24 13:45:22 -0800

    Add files via upload

---
+++ Homework5-Q1.cs
@@ -0,0 +1,23 @@
+﻿namespace Homework5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Q1
+        Console.WriteLine("Enter the first integer:");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the second integer:");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int largest = GetLargest(num1, num2);
+        Console.WriteLine($"a={num1}; b={num2}");
+        Console.WriteLine($"The largest number is: {largest}");
+        
+    }
+    static int GetLargest(int a, int b)
+    {
+        return (a > b) ? a : b;
+    }
+}
---
+++ Homework5-Q2.cs
@@ -0,0 +1,33 @@
+﻿namespace Homework5_Q2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the first integer:");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the second integer:");
+        int num2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the third integer:");
+        int num3 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the fourth integer:");
+        int num4 = int.Parse(Console.ReadLine());
+
+        int largest = GetLargestOfFour(num1, num2, num3, num4);
+        Console.WriteLine($"a={num1}; b={num2}; c={num3}; d={num4}");
+        Console.WriteLine($"The largest number is: {largest}");
+    }
+static int GetLargest(int a, int b)
+    {
+        return (a > b) ? a : b;
+    }
+    static int GetLargestOfFour(int a, int b, int c, int d)
+    {
+        int largestAB = GetLargest(a, b);
+        int largestCD = GetLargest(c, d);
+        return GetLargest(largestAB, largestCD);
+    }
+}
